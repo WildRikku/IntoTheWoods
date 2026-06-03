c88dbc6ac3b934a1d202e3c61988d50eec652690
WildRikku <119439592+WildRikku@users.noreply.github.com>
2026-06-03 18:00:04 +0200

    Make sure game aspect ratio is correct

--- IntoTheWoods-Unity/Assets/Scripts/GameManager.cs
+++ IntoTheWoods-Unity/Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class GameManager : MonoBehaviour {
+    private const float FullHDratio = 0.5625f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Awake() {
+        // Scale camera to show black borders and keep 16:9 aspect ratio
+        float aspect = (float)Screen.height / Screen.width; // do not use currentResolution, it returns the total resolution for all screens on multi-screen setups
+
+        if (aspect > FullHDratio) {
+            Debug.Assert(Camera.main != null, "Camera.main != null");
+            Rect rect = Camera.main.rect;
+            rect.height = FullHDratio * Screen.width / Screen.height;
+            rect.y = (1 - rect.height) / 2;
+            Camera.main.rect = rect;
+        }
     }
 }
