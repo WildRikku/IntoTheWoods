08351eded11d3821a6b015afa59c6b8001b33a42
WildRikku <119439592+WildRikku@users.noreply.github.com>
2026-06-04 15:26:08 +0200

    Implement moving from screen to screen, fix facing direction, change screens to prefabs

--- IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Player.cs
+++ IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Player.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace IntoTheWoods {
+    public delegate bool PlayerWillMoveEventHandler(Player sender, Vector2 direction);
+
     public class Player : MonoBehaviour, InputSystem_Actions.IPlayerActions {
+        [SerializeField] private float speed = 1.7f;
+        [SerializeField] private int jumpForce = 125;
+
         private @InputSystem_Actions _wrapper;
         private Rigidbody2D _rigidbody;
-        [SerializeField] private float speed;
-        [SerializeField] private int jumpForce = 125;
 
         private bool _walking;
         private Vector2 _walkingDirection;
+
+        public event PlayerWillMoveEventHandler WillMove;
 
         private void Start() {
             _rigidbody = GetComponent<Rigidbody2D>();
@@ -22,14 +30,32 @@ namespace IntoTheWoods {
 
         private void Update() {
             if (_walking) {
+                // By sending the event before moving, we have the game manager check if we are going out of bounds
+                // the order is relevant to prevent glitching outside
+                if (!WillMove?.Invoke(this, _walkingDirection) ?? false) {
+                    _walking = false;
+                    return;
+                }
+
                 // zeroing out y axis since that only makes sense for ladders etc
                 transform.position += new Vector3(speed * Time.deltaTime * _walkingDirection.x, 0);
             }
         }
 
         public void OnMove(InputAction.CallbackContext context) {
-            Vector2 moveVector = context.ReadValue<Vector2>();
             if (context.started) {
+                // grab input
+                Vector2 moveVector = DigitizeMovement(context.ReadValue<Vector2>());
+                if (moveVector.x == 0) {
+                    return;
+                }
+
+                // update scale (flip facing direction)
+                Vector3 scale = transform.localScale;
+                scale.x = moveVector.x;
+                transform.localScale = scale;
+
+                // activate walking
                 _walking = true;
                 _walkingDirection = moveVector;
             }
@@ -61,6 +87,17 @@ namespace IntoTheWoods {
         }
 
         public void OnSprint(InputAction.CallbackContext context) {
+        }
+
+        /// <summary>
+        /// Our game does not support analogue movement, so sanitize values from joysticks.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private Vector2 DigitizeMovement(Vector2 input) {
+            float x = input.x > 0 ? 1 : input.x < 0 ? -1 : 0;
+            float y = input.y > 0 ? 1 : input.y < 0 ? -1 : 0;
+            return new(x, y);
         }
     }
 }
