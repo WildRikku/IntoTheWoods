6e5627fd90e92f6470f2af494e754a245bb003cd
WildRikku <119439592+WildRikku@users.noreply.github.com>
2026-06-06 15:01:14 +0200

    use booleans in mouse animator, give mouse a parent container, add one mouse to scene, rename Maus to mouse

---
+++ IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Characters/Mouse.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace IntoTheWoods.Characters {
+    public class Mouse : MonoBehaviour {
+        /// <summary>
+        /// false = front lane, true = back lane
+        /// </summary>
+        public bool backLane;
+    }
+}
