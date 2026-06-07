576a40c406e0abee7cf25edd1a000a9dff5ee88f
daschoe <daschoe@tuta.io>
2026-06-07 11:28:54 +0200

    Fixed Mouse facing direction bug

--- IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Characters/Mouse.cs
+++ IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Characters/Mouse.cs
@@ -17,12 +17,14 @@ namespace IntoTheWoods.Characters {
         private Vector2 _target;
         private Vector2 _direction;
         private bool _moving;
+        private bool _lookingLeft;
         private Action _afterEatingAction;
 
         private void Start() {
             Assert.IsNotNull(head);
             Assert.IsNotNull(animator);
             Assert.IsNotNull(animatorSignal);
+            _lookingLeft = true;
         }
 
         private void OnEnable() {
@@ -54,18 +56,25 @@ namespace IntoTheWoods.Characters {
 
         public void Call(Vector2 position, Action afterEating) {
             _target = position;
+            Debug.Log(transform.position.ToString() + _lookingLeft.ToString() +position.ToString());
+            Vector3 scale = transform.localScale;
             if (position.x < transform.position.x) {
                 _direction = new(-1, 0);
+                if (!_lookingLeft) {
+                    Debug.Log("Flipping left"+scale.x);
+                    _lookingLeft = true;
+                    scale.x *= -1;
+                }
             }
             else {
                 _direction = new(1, 0);
-                Vector3 scale = transform.localScale;
-                if (Math.Sign(scale.x) == 1) {
+                if (_lookingLeft) {
+                    Debug.Log("Flipping right"+scale.x);
+                    _lookingLeft = false;
                     scale.x *= -1; // ensure to just flip, not change scale 
                 }
-
-                transform.localScale = scale;
             }
+            transform.localScale = scale;
 
             _moving = true;
             animator.SetBool(Running, true);
