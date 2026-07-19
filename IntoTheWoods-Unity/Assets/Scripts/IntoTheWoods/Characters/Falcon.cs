86d5d2d013ee1afc7a6470cccfbd3554043f0d16
WildRikku <119439592+WildRikku@users.noreply.github.com>
2026-07-20 00:12:54 +0200

    complete code for basic Falcon moving for all the animations. Falcon sees mouse, Falcon lands, rises, departs. Add related conditions in animator controller.

--- IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Characters/Falcon.cs
+++ IntoTheWoods-Unity/Assets/Scripts/IntoTheWoods/Characters/Falcon.cs
@@ -6,7 +6,10 @@ namespace IntoTheWoods.Characters {
     public class Falcon : MonoBehaviour {
         private static readonly int Attacking = Animator.StringToHash("attacking");
         private static readonly int Captured = Animator.StringToHash("captured");
+        private static readonly int Landed = Animator.StringToHash("landed");
+        private static readonly int Rising = Animator.StringToHash("rising");
         private const float DefaultHeight = 1.495f;
+        private const float WaitOnGroundTime = 1.5f;
         [Header("Patrouille")]
         public float leftEnd;
         public float rightEnd;
@@ -17,17 +20,28 @@ namespace IntoTheWoods.Characters {
         [ShowInInspector] private Vector2 _flyingDirection = new(-1, 0);
         [SerializeField] private Animator animator;
         [ShowInInspector] private bool _flyingToTarget;
+        [ShowInInspector] private bool _waiting;
+        private float _waitedTime;
+        [ShowInInspector] private bool _rising;
         [ShowInInspector] private Vector3 _currentTarget;
+        /// <summary>
+        /// Indicates if the target is a mouse (if not, it's probably a character).
+        /// Does not contain a meaningful value when <see cref="_flyingToTarget"/> is false (does not get reset after reaching target).
+        /// </summary>
         [ShowInInspector] private bool _targetIsMouse = true;
 
         private void Update() {
-            Vector3 posDelta;
+            Vector3 posDelta = new(speed * Time.deltaTime * _flyingDirection.x, 0); // default flying
             if (_flyingToTarget) {
                 Vector3 distance = _currentTarget - transform.position;
                 if (Math.Abs(distance.magnitude) < 0.1f) {
                     // reached target
                     _flyingToTarget = false;
-                    posDelta = new(speed * Time.deltaTime * _flyingDirection.x, 0); // TODO rise again
+                    animator.SetBool(Attacking, false);
+                    animator.SetBool(Landed, true);
+                    _waiting = true;
+                    _waitedTime = 0;
+                    // EditorApplication.isPaused = true;
                 }
                 else {
                     if (Math.Abs(distance.x) > 5) {
@@ -39,6 +53,31 @@ namespace IntoTheWoods.Characters {
                         animator.SetBool(Attacking, true);
                         posDelta = distance.normalized * (speed * Time.deltaTime);
                     }
+                }
+            }
+            else if (_waiting) {
+                // wait on ground to pick on mouse and give players a little time to capture the bird
+                _waitedTime += Time.deltaTime;
+                if (_waitedTime > WaitOnGroundTime) {
+                    _waiting = false;
+                    _rising = true;
+                    animator.SetBool(Rising, true);
+                    _currentTarget = new(transform.position.x + 5, DefaultHeight, 0); // TODO respect facing direction
+                    Destroy(currentMouse);
+                }
+                else {
+                    posDelta = Vector3.zero;
+                }
+            }
+            else if (_rising) {
+                // get back to flying height
+                Vector3 distance = _currentTarget - transform.position;
+                if (Math.Abs(distance.magnitude) < 0.1f) {
+                    animator.SetBool(Rising, false);
+                    _rising = false;
+                }
+                else {
+                    posDelta = distance.normalized * (speed * Time.deltaTime);
                 }
             }
             else {
@@ -55,8 +94,6 @@ namespace IntoTheWoods.Characters {
                     scale.x *= -1;
                     transform.localScale = scale;
                 }
-
-                posDelta = new(speed * Time.deltaTime * _flyingDirection.x, 0);
             }
 
             transform.position += posDelta;
@@ -75,11 +112,10 @@ namespace IntoTheWoods.Characters {
 
                 if (mouse != null && mouse.mouseActive && !mouse._moving) {
                     Debug.Log("SPOTTED MOUSE " + mouse.gameObject.name + " in " + mouse.transform.parent.gameObject.name);
-                    // animator.SetBool(Attacking, false);
-                    // animator.SetBool(Captured, true);
-                    // currentTarget = new(transform.position.x - 5f, DefaultHeight, 0);
                     _currentTarget = mouse.gameObject.transform.position;
+                    currentMouse = mouse;
                     _flyingToTarget = true;
+                    // Time.timeScale = 0.5f; // TODO DEBUG
                 }
             }
             else if (other.CompareTag("Player")) {
